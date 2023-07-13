565edefac7a43118439771c595d697ff68b88401
Seid Mohammed <seid.seid1@ju.edu.et>
2023-07-13 16:57:00 +0300

    Butto updated

--- resources/views/app/lab_test_request_groups/show.blade.php
+++ resources/views/app/lab_test_request_groups/show.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
+ 
+<div class="card  sborder-primary-left-5  border border-primary">
+ 
+
+        {{-- <div class="card  collapsed-card border-primary-left-5  border border-primary">
+  --}}
+
+            <div class="card-header">
+       
+                <b>  <i  class="fas fa-list"></i> Incounter Information  </b>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus   "></i>
+                    </button>
+        
+                </div>
+             
+            </div>
+
         <div class="card">
             <div class="card-body">
                 {{-- <h4 class="card-title">
@@ -10,127 +27,65 @@
                     @lang('crud.lab_test_request_groups.show_title')
                 </h4> --}}
 
-
-             
-                     
-                       
                         <div class="row m-2">
                 
+                   
                             <div class="input-group mb-3 col-md-4">
-                
-                
-                              
-                                    <span title=""><i  class="fas fa-user"></i></span>
-                          
-                                {{-- <h3> Status </h3> --}}
-                                <span>{{ $labTestRequestGroup->status ?? '-' }} </span>
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">Encounter Status </span>
+                                <span>&nbsp;&nbsp;&nbsp;&nbsp; {{ $labTestRequestGroup->status ?? '-' }} </span>
                 
                             </div>
                             <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Second Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
-                                <span>{{ $labTestRequestGroup->status ?? '-' }} </span>
-                
-                            </div>
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">Encounter priority </span>
                                 <span>{{ $labTestRequestGroup->priority ?? '-' }}</span>
                 
                             </div>
 
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
+                            {{-- <div class="input-group mb-3 col-md-4">
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">Encounter Status </span>
                                 <span>{{ $labTestRequestGroup->notification ?? '-' }}</span>
                 
-                            </div>
+                            </div> --}}
+
+
+                       
+
+
+    
 
 
                             <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
-                                <span>{{ $labTestRequestGroup->status ?? '-' }} </span>
-                
-                            </div>
-
-
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
-                                <span>{{ $labTestRequestGroup->priority ?? '-' }}</span>
-                
-                            </div>
-
-
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
-                                <span>{{ $labTestRequestGroup->notification ?? '-' }}</span>
-                
-                            </div>
-
-
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">Encounter call status </span>
                                 <span>{{ $labTestRequestGroup->call_status ?? '-' }}</span>
                 
                             </div>
 
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
-                                <span>{{ $labTestRequestGroup->call_status ?? '-' }}</span>
-                
-                            </div>
-
+                      
 
                             <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">Date of request  </span>
                                 <span>{{ $labTestRequestGroup->requested_at ?? '-' }}</span>
                             </div>
 
 
                             <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">Doctor </span>
                                 <span>{{ optional($labTestRequestGroup->Requestedby)->id ?? '-' }}</span>
                             </div>
 
 
                             <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text"><i  class="fas fa-user"></i></span>
-                                </div>
+                                <i class="fa fa-caret-right"> </i>&nbsp;
+                                <span title="">patient  </span>
                                 <span>{{  optional($labTestRequestGroup->encounter)->student->fullName ?? '-' }}</span>
                             </div>
 
-
-                            <div class="input-group mb-3 col-md-4">
-                                <div class="input-group-prepend">
-                                    <span title="Last Name" class="input-group-text">
-                                        
-                                    
-                                        <h5>
-                                            Patient
-                                         </h5>
-                                    
-                                    </span>
-                                </div>
-                                <span>{{  optional($labTestRequestGroup->encounter)->student->fullName ?? '-' }}</span>
-                            </div>
 
 
 
@@ -138,50 +93,6 @@
                         </div>
                     
             
-
-{{-- 
-                <div class="mt-4">
-                    <div class="mb-4">
-                        <h5>@lang('crud.lab_test_request_groups.inputs.status')</h5>
-                        <span>{{ $labTestRequestGroup->status ?? '-' }} </span>
-                    </div>
-                    <div class="mb-4">
-                        <h5>
-                            @lang('crud.lab_test_request_groups.inputs.priority')
-                        </h5>
-                        <span>{{ $labTestRequestGroup->priority ?? '-' }}</span>
-                    </div>
-                    <div class="mb-4">
-                        <h5>
-                            @lang('crud.lab_test_request_groups.inputs.notification')
-                        </h5>
-                        <span>{{ $labTestRequestGroup->notification ?? '-' }}</span>
-                    </div>
-                    <div class="mb-4">
-                        <h5>
-                            @lang('crud.lab_test_request_groups.inputs.call_status')
-                        </h5>
-                        <span>{{ $labTestRequestGroup->call_status ?? '-' }}</span>
-                    </div>
-                    <div class="mb-4">
-                        <h5>
-                            @lang('crud.lab_test_request_groups.inputs.requested_at')
-                        </h5>
-                        <span>{{ $labTestRequestGroup->requested_at ?? '-' }}</span>
-                    </div>
-                    <div class="mb-4">
-                        <h5>
-                            @lang('crud.lab_test_request_groups.inputs.clinic_user_id')
-                        </h5>
-                        <span>{{ optional($labTestRequestGroup->Requestedby)->id ?? '-' }}</span>
-                    </div>
-                    <div class="mb-4">
-                        <h5>
-                           Patient
-                        </h5>
-                        <span>{{  optional($labTestRequestGroup->encounter)->student->fullName ?? '-' }}</span>
-                    </div>
-                </div> --}}
 
                 <div class="mt-4">
                     <a href="{{ route('lab-test-request-groups.index') }}" class="btn btn-sm btn-outline-primary float-right mx-1">
