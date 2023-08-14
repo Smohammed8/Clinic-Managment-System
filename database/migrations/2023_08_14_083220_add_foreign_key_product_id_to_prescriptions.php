759c0cf519dca7dc68079c8ebb5fcfdb7bd96f16
Abraham <adem.sisay@ju.edu.et>
2023-08-14 11:43:08 +0300

    producnt id to presicrption

---
+++ database/migrations/2023_08_14_083220_add_foreign_key_product_id_to_prescriptions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prescriptions', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('product_id')->default(0)->nullable();
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prescriptions', function (Blueprint $table) {
+            //
+            $table->dropColumn('product_id');
+        });
+    }
+};
