9e1f8a99a25be885c2c286f8c1fcf8e00b01c327
Abraham <adem.sisay@ju.edu.et>
2023-08-15 12:10:01 +0300

    queue web modified

---
+++ app/Http/Controllers/QueueController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QueueController extends Controller
+{
+    //
+    public function getOPDQueue()
+    {
+        dd('getOPDQueue');
+    }
+    public function getLabQueue()
+    {
+        dd('getLabQueue');
+    }
+}
