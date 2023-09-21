cce4e48fe49cad24aa2d2fd7762899617dbfdfab
asisay <adem.abraham@ju.edu.et>
2023-09-21 14:49:24 +0300

    Ldap fixed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use App\Helper\LdapHelper;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -36,5 +41,34 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    public function login(Request $request, LdapHelper $ldapHelper)
+    {
+        $credentials = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required',
+        ]);
+
+        if ($request->username == "admin") {
+            if (Auth::attempt($credentials)) {
+                return redirect(route('dashboard'));
+            } else {
+                return Redirect::back()->withErrors(['msg' => 'Invalid Credentials']);
+            }
+        } else {
+
+            $auth = $ldapHelper->authenticate($credentials);
+            //dd($auth);
+            if ($auth) {
+
+                Auth::login($auth);
+                session()->regenerate();
+                return redirect(route('dashboard'));
+            } else {
+                return Redirect::back()->withErrors(['msg' => 'Invalid Credentials']);
+            }
+        }
     }
 }
