7cae054f342d867ea58277608e424f694f266a7d
Abraham Sisay <adem.abraham@ju.edu.et>
2023-11-03 10:38:17 +0300

    seid to pull

--- resources/views/app/queue/opd.blade.php
+++ resources/views/app/queue/opd.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="container-fluid" style="height: 100vh;">
         <div class="row h-100">
-            <div class="col-12" style="height: 65%;">
-                <h1>Queue</h1>
+            <h1>Queue</h1>
+            <div class="col-12 row " style="height: 65%;">
                 <div class="p-2 col-7">
                     {{ Widget::run('opdQueueCard') }}
                 </div>
-                <div class="p-2 col-3">
+                <div class="p-2 col-5">
                 </div>
             </div>
             <div class="col-md-12 bg-info" style="height: 35%;">
