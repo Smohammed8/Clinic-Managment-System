8c61386d53b410413dd42fd499908017280698f2
Abraham Sisay <adem.abraham@ju.edu.et>
2023-11-03 21:41:11 +0300

    SRS controlle rto be fixed later

--- app/Http/Controllers/SRSController.php
+++ app/Http/Controllers/SRSController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Console\View\Components\Alert;
 use PDOException;
 use Illuminate\Http\Request;
@@ -11,7 +10,6 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Response;
 
-require_once app_path('Helper/constants.php');
 class SRSController extends Controller
 {
 
@@ -47,118 +45,62 @@ class SRSController extends Controller
 
     public function unit()
     {
-        $sis = DB::connection('mysql');
+    }
+    public function insert()
+    {
+
         try {
-            $srs = DB::connection('mysql_srs')->getPdo();
-            //  dd("The connection is successful");
-            
-            
-            
-            $data = DB::connection('mysql_srs')->select("SELECT id, program_level_name as name FROM program_level");
-//   dd($data);
-            try {
-                foreach ($data as $value) {
-                    // dd($data);
-                    // dd("herse");
-                    $value = (array) $value;
+            $dbcon = DB::connection('mysql_srs');
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+        }
 
-                    try {
-    DB::statement("INSERT INTO program_level (id, name) VALUES (:id, :name) ON DUPLICATE KEY UPDATE name = :name", $value);
-                    } catch (\Throwable $th) {
-                        dd($th->getMessage());
-                    }
-                }
-                Session::flash('success', 'Program Level Successfully Synchronized');
-            } catch (\Throwable $th) {
-                Session::flash('danger', 'Error Occurred While Fetching Program Level');
-            }
+        $results = $dbcon->select(DB::raw(" SELECT s.id, sf.username, ifo.program_id, sf.first_name, sf.fathers_name, sf.grand_fathers_name, s.student_id, s.sex,
+        IFNULL(sd.photo, '') AS photo, ifo.academic_year, ifo.year, 1,
+        IF(ifo.is_registered = 1, 1, 0) AS is_registered, s.admission_year, 'Ethiopian' AS nationality, ifo.section, ifo.semester, s.birth_date,
+        sd.entrance_exam_id, sd.mother_name AS adopter_name,
+        sd.zone_id AS school_zone, sd.woreda AS school_woreda, sd.kebele AS school_kebele,
+        sd.telephone AS phone, sd.family_phone
+        FROM student s
+        INNER JOIN sf_guard_user sf ON sf.id = s.sf_guard_user_id
+        INNER JOIN student_info ifo ON s.id = ifo.student_id
+        JOIN student_detail sd ON s.id = sd.student_id
+        WHERE ifo.record_status = 1 AND s.id != 0 AND ifo.year != 0
+        ORDER BY ifo.id DESC;
+        -- LIMIT 100; 
+    "));
 
-            dd("finshed");
-
-            $sql = "SELECT id, program_level_name as name FROM program_level ";
-            $stmt = $srs->prepare($sql);
-            $stmt->execute();
-            // dd($stmt);
-            $data = $stmt->fetchAll();
-            // dd($data);
-            // $statement = $srs->prepare('INSERT INTO users (name, email) VALUES (?, ?)');
-            $sql = "INSERT INTO program_level (id,name) Values(:id,:name) ON DUPLICATE KEY UPDATE name = :name";
-            $stmt = $sis->prepare($sql);
+        $data = $results;
+        $targetTable = 'students';
+        foreach ($data as $value) {
+            // Convert the result object to an associative array
+            $value = (array) $value;
             try {
-                foreach ($data as $value) {
-                    $stmt->execute($value);
-                }
-            } catch (\Throwable $th) {
-                Session::flash('danger', "Error Occured While Fetching Program Level ");
-            }
-            Session::flash("success", "Program Level Successfully Syncronized");
-
-            /*
-            * Progam Type Sync
-            */
-            $sql = "SELECT id,program_type_name as name FROM program_type where id!=0 ";
-            $stmt = $srs->prepare($sql);
-            $stmt->execute();
-            $data = $stmt->fetchAll();
-            // dd($data);
-            $sql = "INSERT INTO program_type (id,name) Values(:id,:name) ON DUPLICATE KEY UPDATE name = :name";
-            $stmt = $sis->prepare($sql);
-            try {
-                foreach ($data as $value) {
-                    $stmt->execute($value);
-                }
-            } catch (\Throwable $th) {
-                Session::flash('danger', "Error Occured While Fetching Program Type ");
-            }
-            Session::flash("success", "Program Type Successfully Syncronized");
-            /*
-    * Enrollment Type Sync
-    */
-            $sql = "SELECT id,enrollment_type_name as name FROM  enrollment_type where id!=0 ";
-            $stmt = $srs->prepare($sql);
-            $stmt->execute();
-            $data = $stmt->fetchAll();
-            $sql = "INSERT INTO enrollment_type (id,name) Values(:id,:name) ON DUPLICATE KEY UPDATE name = :name";
-            $stmt = $sis->prepare($sql);
-            try {
-                foreach ($data as $value) {
-                    $stmt->execute($value);
-                }
-            } catch (\Throwable $th) {
-                Session::flash('danger', "Error Occured While Fetching Enrollment Type ");
-            }
-            Session::flash("success", "Enrollment Type Successfully Syncronized");
-            /*
-    * College Sync
-    */
-            $sql = "SELECT id,college_name as name FROM college where id!=0";
-            $stmt = $srs->prepare($sql);
-            $stmt->execute();
-            $data = $stmt->fetchAll();
-            $sql = "INSERT INTO college (id,name) Values(:id,:name) ON DUPLICATE KEY UPDATE name = :name";
-            $stmt = $sis->prepare($sql);
-            try {
-                foreach ($data as $value) {
-                    $stmt->execute($value);
-                }
-            } catch (\Throwable $th) {
-                Session::flash('danger', "Error Occured While Fetching College ");
-            }
-            Session::flash("success", "College Successfully Syncronized");
-
-            /*
-    * Department Sync
-    */
-            $sql = "SELECT id,college_id,department_name as name FROM department where id!=0";
-            $stmt = $srs->prepare($sql);
-            $stmt->execute();
-            $data = $stmt->fetchAll();
-            $sql = "INSERT INTO department (id,college_id,name) Values(:id,:college_id,:name) ON DUPLICATE KEY UPDATE college_id=:college_id, name = :name";
-            $stmt = $sis->prepare($sql);
-            try {
-                foreach ($data as $value) {
-                    $stmt->execute($value);
-                }
+                $moh = DB::connection('mysql');
+                $result = $moh->table($targetTable)->updateOrInsert(
+                    ['id_number' => $value['id']],
+                    [
+                        //'username' => $value['username'],
+                        'first_name' => $value['first_name'],
+                        'middle_name' => $value['fathers_name'],
+                        'last_name' => $value['grand_fathers_name'],
+                        'sex' => $value['sex'],
+                        'program_id' => $value['program_id'],
+                        'photo' => $value['photo'],
+                        'academic_year' => $value['academic_year'],
+                        'year' => $value['year'],
+                        //'campus_id'=>$value['campus_id'],
+                        'year_of_entrance' => $value['admission_year'],
+                        'is_student_active' => 1,
+                        'mobile_number' => $value['phone'],
+                        'is_registered' => $value['is_registered'],
+                        'nationality' => $value['nationality'],
+                        'section' => $value['section'],
+                        'semester' => $value['semester'],
+                        'date_of_birth' => $value['birth_date'],
+                        'entrance_reg_no' => $value['entrance_exam_id'],
+                    ]
+                );
             } catch (\Throwable $th) {
                 dd($th->getMessage());
             }
@@ -169,18 +111,85 @@ class SRSController extends Controller
     }
     //////////////////////////////////////////////////////////////////////////////
 
-    
-    public function syncDepartment()
-    {
-    }
-    public function syncProgram()
-    {
-    }
+    // public function syncProgramRelated()
+    // {
+    //     $programTypeQuery = $this->conn->table('program_type')->select('program_type.id as program_type_id', 'program_type.program_type_name')->get();
 
-    public function syncProgramType()
-    {
-    }
-    public function syncProgramLevel()
-    {
-    }
+    //     $programLevelQuery = $this->conn->table('program_level')->select('program_level.id as program_level_id', 'program_level.program_level_name')->get();
+
+    //     $enrollmentTypeQuery = $this->conn->table('enrollment_type')->select('enrollment_type.id as enroll_type_id', 'enrollment_type.enrollment_type_name')->get();
+
+    //     foreach ($programTypeQuery as $key => $proType) {
+    //         $proType = (array) $proType;
+    //         ProgramType::updateOrCreate(['id' => $proType['program_type_id']], ['id' => $proType['program_type_id'], 'name' => $proType['program_type_name']]);
+    //     }
+
+    //     foreach ($programLevelQuery as $key => $proLevel) {
+    //         $proLevel = (array) $proLevel;
+    //         ProgramLevel::updateOrCreate(['id' => $proLevel['program_level_id']], ['id' => $proLevel['program_level_id'], 'name' => $proLevel['program_level_name']]);
+    //     }
+
+    //     foreach ($enrollmentTypeQuery as $key => $enrType) {
+    //         $enrType = (array) $enrType;
+    //         EnrollmentType::updateOrCreate(['id' => $enrType['enroll_type_id']], ['id' => $enrType['enroll_type_id'], 'name' => $enrType['enrollment_type_name']]);
+    //     }
+    // }
+
+    // public function fetch()
+    // {
+    //     // $this->syncProgramRelated();
+    //     $query = $this->conn->table('college')
+    //         ->join('department', 'college.id', '=', 'department.college_id')
+    //         ->join('program', 'department.id', '=', 'program.department_id')
+    //         ->select('college.id as college_id', 'college.college_name', 'department.id as department_id', 'department.department_name', 'program.id as program_id', 'program.name as program_name', 'program.enrollment_type_id', 'program.program_level_id', 'program.program_type_id')
+    //         ->get();
+
+    //     $collegesWithDepartments = [];
+
+    //     foreach ($query as $result) {
+    //         $collegeID = $result->college_id;
+    //         $collegeName = $result->college_name;
+    //         $departmentID = $result->department_id;
+    //         $departmentName = $result->department_name;
+
+    //         $programID = $result->program_id;
+    //         $programName = $result->program_name;
+    //         $programEnrollment = $result->enrollment_type_id;
+    //         $programType = $result->program_type_id;
+    //         $programLevel = $result->program_level_id;
+
+    //         // Store the college name and associated departments
+    //         if (!isset($collegesWithDepartments[$collegeID])) {
+    //             $collegesWithDepartments[$collegeID] = [
+    //                 'college_name' => $collegeName,
+    //                 'departments' => [],
+    //             ];
+    //         }
+
+    //         $collegesWithDepartments[$collegeID]['departments'][] = [
+    //             'department_id' => $departmentID,
+    //             'department_name' => $departmentName,
+    //         ];
+    //     }
+
+    //     foreach ($collegesWithDepartments as $key => $collegesWithDepartment) {
+    //         if ($key == 0) {
+    //             continue;
+    //         }
+    //         $college = College::updateOrCreate(['id' => $key], ['id' => $key, 'name' => $collegesWithDepartment['college_name']]);
+
+    //         foreach ($collegesWithDepartment['departments'] as $dep) {
+    //             $dep =  Department::updateOrCreate(['id' => $dep['department_id']], ['id' => $dep['department_id'], 'name' => $dep['department_name'], 'college_id' => $college->id]);
+    //             $dep->getDepartmentSessionStatus($this->latestAcademicSession);
+
+    //             $this->syncProgram($dep->id);
+    //             $this->syncInstructor($dep->id);
+    //             try {
+    //                 $this->assignDepartmentHead($dep->id);
+    //             } catch (Exception $e) {
+    //                 continue;
+    //             }
+    //         }
+    //     }
+    // }
 }
