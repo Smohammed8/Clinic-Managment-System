5d853c3a910fcf483b2ddc661d77b2817a23b379
Abraham Sisay <adem.abraham@ju.edu.et>
2023-11-06 13:47:35 +0300

    aa

--- database/migrations/2023_11_02_141451_create_departments_table.php
+++ database/migrations/2023_11_02_141451_create_departments_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Collage::class, 'college_id')->constrained()->nullable();
-            $table->string('department_id')->nullable();
             $table->string('name')->nullable();
+            $table->string('department_id')->nullable();
+            $table->string('collage_id')->nullable();
             $table->timestamps();
         });
     }
