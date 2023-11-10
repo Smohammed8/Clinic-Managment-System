06dc2fd93f18e066ccc7ecb7dca5894e08b4e3fe
Bilkes Elias <67165306+BiluAilu@users.noreply.github.com>
2023-11-10 06:48:11 +0300

    stock category fix

--- resources/views/app/stock_categories/form-inputs.blade.php
+++ resources/views/app/stock_categories/form-inputs.blade.php
@@ -1,3 +1,52 @@
 @php $editing = isset($stockCategory) @endphp
+<div class="row">
+    <x-inputs.group class="col-sm-12">
+        <x-inputs.text
+            name="name"
+            label="Name"
+            :value="old('name', ($editing ? $stockCategory->name : ''))"
+            maxlength="255"
+            placeholder="Name"
+        ></x-inputs.text>
+    </x-inputs.group>
 
-<div class="row"></div>
+    {{-- <x-inputs.group class="col-sm-12">
+        <x-inputs.select name="campus_id" label="Campus">
+            @php $selected = old('campus_id', ($editing ? $store->campus_id : '')) @endphp
+            <option disabled {{ empty($selected) ? 'selected' : '' }}>Please select the Campus</option>
+            @foreach($campuses as $value => $label)
+            <option value="{{ $value }}" {{ $selected == $value ? 'selected' : '' }} >{{ $label }}</option>
+            @endforeach
+        </x-inputs.select>
+    </x-inputs.group> --}}
+
+    <x-inputs.group class="col-sm-12">
+        <x-inputs.textarea
+            name="description"
+            label="Description"
+            maxlength="255"
+            >{{ old('description', ($editing ? $stockCategory->description : ''))
+            }}</x-inputs.textarea
+        >
+    </x-inputs.group>
+
+    {{-- <x-inputs.group class="col-sm-12">
+        <x-inputs.text
+            name="status"
+            label="Status"
+            :value="old('status', ($editing ? $store->status : ''))"
+            maxlength="255"
+            placeholder="Status"
+            required
+        ></x-inputs.text>
+    </x-inputs.group> --}}
+    {{-- <x-inputs.group class="col-sm-12">
+        <x-inputs.checkbox
+            name="status"
+            label="Status"
+            :checked="old('status', ($editing ? $store->status : 0))"
+        ></x-inputs.checkbox>
+    </x-inputs.group> --}}
+
+</div>
+
