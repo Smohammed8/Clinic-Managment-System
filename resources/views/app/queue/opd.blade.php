171994c5deb0bf9a927afd33de69a054716d3c7d
Abrham Sisay <adem.abraham@ju.edu.et>
2023-11-18 15:47:58 +0300

    que table time counter added

--- resources/views/app/queue/opd.blade.php
+++ resources/views/app/queue/opd.blade.php
@@ -48,6 +48,27 @@
                                                                 @else
                                                                     Waiting
                                                                 @endif
+                                                                <span id="timeCounter" class="right badge badge-danger">
+                                                                    @if ($encounter->created_at)
+                                                                        @php
+                                                                            $acceptedTime = \Carbon\Carbon::parse($encounter->created_at);
+                                                                            $diffInMinutes = $acceptedTime->diffInMinutes();
+                                                                            $diffInHours = $acceptedTime->diffInHours();
+                                                                            $diffInDays = $acceptedTime->diffInDays();
+                                                                        @endphp
+
+                                                                        @if ($diffInMinutes < 60)
+                                                                            {{ $diffInMinutes }} minutes ago
+                                                                        @elseif($diffInHours < 24)
+                                                                            {{ $diffInHours }} hours ago
+                                                                        @else
+                                                                            {{ $diffInDays }} days ago
+                                                                        @endif
+                                                                    @else
+                                                                        /
+                                                                    @endif
+                                                                </span>
+
                                                             </p>
                                                         </td>
                                                     @endforeach
